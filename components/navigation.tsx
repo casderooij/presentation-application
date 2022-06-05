e7ecb1250d11147835b69d03e153e40d90d5364b
Cas de Rooij <hello@casderooij.nl>
2022-06-05 14:12:31 +0200

    feat(navigation-component): simple HTML structure (#1)
    * feat(navigation-component): simple HTML structure
    
    * feat(navigation-component): add styles to links

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,13 +2,21 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav>
-      <Link href="/">
-        <a>/</a>
-      </Link>
-      <Link href="/info">
-        <a>/info</a>
-      </Link>
+    <nav className="absolute w-full h-full p-4">
+      <ul className="flex flex-col items-end justify-between h-full">
+        <li>
+          <Link href="/">
+            <a className="px-6 py-2 text-white rounded-full bg-slate-800">/</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/info">
+            <a className="px-6 py-2 text-white rounded-full bg-slate-800/60 backdrop-blur-lg">
+              /info
+            </a>
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
