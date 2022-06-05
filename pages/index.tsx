55f49b885c87a9caa0f526384bd708561cc0dbc0
Cas de Rooij <hello@casderooij.nl>
2022-06-05 12:32:40 +0200

    feat(navigation-component): simple HTML structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Navigation from "../components/navigation";
 
 const Home: NextPage = () => {
   return (
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Navigation />
       <main className="text-red">Hello world</main>
     </div>
   );
