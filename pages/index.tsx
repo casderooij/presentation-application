f7aabf34afac98b3d27fa8edf9a5e2a0de82a369
Cas de Rooij <hello@casderooij.nl>
2022-06-07 14:19:13 +0200

    feat(index.tsx): create multiple Window components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Window from "../components/Window";
 
 const Home: NextPage = () => {
+  const [currentWindows, setCurrentWindows] = useState([{}, {}]);
+
   return (
     <>
       <Head>
         <title>Cas de Rooij</title>
       </Head>
 
-      <Window />
-      <Window />
-      <Window />
+      {currentWindows.map((w, i) => (
+        <Window key={i} />
+      ))}
     </>
   );
 };
