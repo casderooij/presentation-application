cc78508ff0d0c01c7732f7259ef9ac597b2b4367
Cas de Rooij <hello@casderooij.nl>
2022-06-11 15:29:22 +0200

    feat(tailwind css): install and configure tailwind css (#7)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,6 @@
 <script lang="ts">
   import Box from '$lib/components/Box.svelte'
   import Container from '$lib/components/Container.svelte'
-  import { spring } from 'svelte/motion'
-
-  let coords = spring(
-    { x: 50, y: 50 },
-    {
-      stiffness: 0.1,
-      damping: 0.25
-    }
-  )
-
-  let size = spring(10)
 
   const items = [{ id: 10 }, { id: 4 }, { id: 1 }, { id: 12 }]
 </script>
@@ -23,27 +12,3 @@
     {/each}
   </Container>
 </div>
-
-<svg
-  on:mousemove={e => coords.set({ x: e.clientX, y: e.clientY })}
-  on:mousedown={() => size.set(30)}
-  on:mouseup={() => size.set(10)}
->
-  <circle cx={$coords.x} cy={$coords.y} r={$size} />
-</svg>
-
-<style>
-  .container {
-    width: 100%;
-    height: 100vh;
-    background-color: darksalmon;
-  }
-
-  svg {
-    width: 100%;
-    height: 100vh;
-  }
-  circle {
-    fill: #ff3e00;
-  }
-</style>
