1c71ce1b728e53d46c7e9cc6b8298a23522a6b59
Cas de Rooij <hello@casderooij.nl>
2022-06-19 23:35:54 +0200

    feat: simplify window transition logic

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,22 @@
-import { quintOut } from 'svelte/easing'
-import type { FlyParams, TransitionConfig } from 'svelte/transition'
+export type Project = any
 
 export const makeWindows = (
-  projects: any[],
+  projects: Project[],
   curveFunction: (theta: number) => {
     target: { x: number; y: number }
     size: number
   }
-): any[] => {
+): {
+  project: Project
+  position: {
+    target: { x: number; y: number }
+    offset: { x: number; y: number }
+  }
+  size: number
+}[] => {
   const windowArray = projects.map((project, index) => {
     const { target, size } = curveFunction(index + 1 / projects.length)
-    const offset = { x: 100, y: target.y }
+    const offset = { x: -20, y: 0 }
     return {
       project,
       position: {
@@ -35,60 +41,10 @@ export const makeCurve = (width: number, height: number) => {
 
     return {
       target: {
-        x: Math.round(w + rx * Math.cos(theta * 1)),
-        y: Math.round(h + ry * Math.sin(theta * 1.5))
+        x: Math.round(w + rx * Math.cos(theta * 1) - size / 2),
+        y: Math.round(h + ry * Math.sin(theta * 1.5) - size / 2)
       },
       size
     }
-  }
-}
-
-interface WindowFlyParams extends FlyParams {
-  offset: {
-    x: number
-    y: number
-  }
-  target: {
-    x: number
-    y: number
-  }
-  size: number
-}
-
-export const windowFly = (
-  node: HTMLElement,
-  {
-    delay,
-    duration = 10000,
-    easing = quintOut,
-    offset,
-    target,
-    size,
-    opacity = 0
-  }: WindowFlyParams
-): TransitionConfig => {
-  const style = getComputedStyle(node)
-  const targetOpacity = +style.opacity
-  const halfSize = size / 2
-
-  node.style.transform = `matrix(1, 0, 0, 1, ${target.x - halfSize}, ${
-    target.y - halfSize
-  })`
-  const transform = style.transform === 'none' ? '' : style.transform
-  offset.x -= halfSize
-  offset.y -= halfSize
-
-  const od = targetOpacity * (1 - opacity)
-
-  return {
-    delay,
-    duration,
-    easing,
-    css: (t, u) => `
-        transform: ${transform} translate(${(1 - t) * offset.x}px, ${
-      (1 - t) * offset.y
-    }px);
-        opacity: ${targetOpacity - od * u}
-      `
   }
 }
