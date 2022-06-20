82a72b7b9382a662684a37737704639a0440b86f
Cas de Rooij <hello@casderooij.nl>
2022-06-20 13:21:16 +0200

    Feature/window offset (#17)
    * refactor: add WindowGroup component
    
    * feat: add offset calculation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,7 @@ export const makeWindows = (
   projects: Project[],
   curveFunction: (theta: number) => {
     target: { x: number; y: number }
+    offset: { x: number; y: number }
     size: number
   }
 ): {
@@ -15,8 +16,7 @@ export const makeWindows = (
   size: number
 }[] => {
   const windowArray = projects.map((project, index) => {
-    const { target, size } = curveFunction(index + 1 / projects.length)
-    const offset = { x: -20, y: 0 }
+    const { target, offset, size } = curveFunction(index + 1 / projects.length)
     return {
       project,
       position: {
@@ -33,18 +33,44 @@ export const makeWindows = (
 export const makeCurve = (width: number, height: number) => {
   return (theta: number) => {
     const size = 100
-    const padding = size + 40
+    const padding = size * 2
     const w = width / 2
     const h = height / 2
     const rx = (width - padding) / 2
     const ry = (height - padding) / 2
 
+    const x = Math.round(w + rx * Math.cos(theta * 1.1) - size / 2)
+    const y = Math.round(h + ry * Math.sin(theta * 1.2) - size / 2)
+
+    const offset = getOffset(w, h, x, y)
+
     return {
-      target: {
-        x: Math.round(w + rx * Math.cos(theta * 1) - size / 2),
-        y: Math.round(h + ry * Math.sin(theta * 1.5) - size / 2)
-      },
+      target: { x, y },
+      offset,
       size
     }
+  }
+}
+
+const getOffset = (
+  centerX: number,
+  centerY: number,
+  x: number,
+  y: number
+): { x: number; y: number } => {
+  const vectorToCenter = [centerX - x, centerY - y]
+  const magnitude = Math.sqrt(vectorToCenter[0] ** 2 + vectorToCenter[1] ** 2)
+  const normalizedVectorToCenter = [
+    vectorToCenter[0] / magnitude,
+    vectorToCenter[1] / magnitude
+  ]
+  const oppositeVector = [
+    -normalizedVectorToCenter[0] * 40,
+    -normalizedVectorToCenter[1] * 40
+  ]
+
+  return {
+    x: oppositeVector[0],
+    y: oppositeVector[1]
   }
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,27 @@
 <script lang="ts">
-  /**
-   * Multiple windows with random from - to data
-   * Delay each window (by index)
-   * Maybe reset windows when client resizes window (throttle)
-   */
+  import WindowGroup from '$lib/components/WindowGroup.svelte'
 
-  import { onMount } from 'svelte'
-  import TransitionWindow from '$lib/components/TransitionWindow.svelte'
-  import Window from '$lib/components/Window.svelte'
+  const projects = [
+    { title: '1' },
+    { title: '2' },
+    { title: '3' },
+    { title: '3' },
+    { title: '3' },
+    { title: '3' },
+    { title: '3' },
+    { title: '3' },
+    { title: '3' },
+    { title: '3' }
+  ]
 
+  let width: number, height: number
   let mounted = false
-  let position = {
-    start: {
-      x: -0.2,
-      y: -0.1
-    },
-    end: {
-      x: 0.5,
-      y: 0.5
-    }
-  }
-
-  onMount(() => (mounted = true))
 </script>
 
-<div class="bg-red-300 h-screen relative overflow-hidden">
-  {#if mounted}
-    <TransitionWindow {position}>
-      <Window ratio="2 / 2" minWidth={100} maxWidth={400} />
-    </TransitionWindow>
-  {/if}
-</div>
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+
+<input type="checkbox" bind:checked={mounted} />
+
+{#if mounted}
+  <WindowGroup {projects} {width} {height} />
+{/if}
