3807e7892d3b3d6efe8c444314837972564669f4
Cas de Rooij <hello@casderooij.nl>
2022-06-21 22:12:28 +0200

    refactor(Window.svelte): remove unused code

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -4,32 +4,14 @@
 
   import { DragGesture } from '@use-gesture/vanilla'
 
-  export let ratio = '1 / 1'
-  export let minWidth = 100
-  export let maxWidth = 400
   export let size: number
 
   const minViewPort = 320
   const maxViewPort = 1200
 
-  let widthClamp: string
-
-  $: {
-    const variablePart = (maxWidth - minWidth) / (maxViewPort - minViewPort)
-    const constant = parseFloat(
-      (maxWidth - maxViewPort * variablePart).toFixed(3)
-    )
-    widthClamp = `clamp(${minWidth}px,${
-      constant ? ` ${constant}rem +` : ''
-    } ${parseFloat((100 * variablePart).toFixed(2))}vw, ${maxWidth}px)`
-  }
-
   let isActive = false
   let coords = spring<{ x: number; y: number; opacity?: number }>(
-    {
-      x: 0,
-      y: 0
-    },
+    { x: 0, y: 0 },
     {
       stiffness: 0.02,
       damping: 0.15
@@ -52,9 +34,5 @@
 <div
   bind:this={element}
   class="bg-red-200 touch-none cursor-move pointer-events-auto shadow-lg shadow-black/20"
-  style="
-    width: {size}px;
-    aspect-ratio: {ratio};
-    transform: translate({$coords.x}px, {$coords.y}px);
-  "
+  style="width: {size}px; transform: translate({$coords.x}px, {$coords.y}px);"
 />
