2632c2fd45145cc575612b76c93089af7ab37210
Cas de Rooij <hello@casderooij.nl>
2022-06-21 14:13:28 +0200

    feat: use lodash debounce to reset windows on resize

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,42 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+  import { debounce } from 'lodash'
   import WindowGroup from '$lib/components/WindowGroup.svelte'
 
-  const projects = [
-    { title: '1' },
-    { title: '2' },
-    { title: '3' },
-    { title: '1' },
-    { title: '2' },
-    { title: '3' }
-  ]
+  let projects: any[] = []
 
   let width: number, height: number
   let mounted = false
+
+  onMount(() => {
+    projects = [
+      { title: '1' },
+      { title: '2' },
+      { title: '3' },
+      { title: '1' },
+      { title: '2' },
+      { title: '3' }
+    ]
+  })
+
+  const reset = () => {
+    projects = [
+      { title: '1' },
+      { title: '2' },
+      { title: '3' },
+      { title: '1' },
+      { title: '2' },
+      { title: '3' }
+    ]
+  }
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+<svelte:window
+  bind:innerWidth={width}
+  bind:innerHeight={height}
+  on:resize={debounce(reset, 300)}
+/>
 
-<input type="checkbox" bind:checked={mounted} />
-
-{#if mounted}
+{#key projects}
   <WindowGroup {projects} viewport={{ width, height }} />
-{/if}
+{/key}
